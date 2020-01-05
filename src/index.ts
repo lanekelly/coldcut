a6aaa480228ac3be59653b51e0ce5a59943d9cf1
Lane Kelly <lanekelly16@gmail.com>
2020-01-04 17:36:11 -0800

    complete rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,179 +1,225 @@
-import { Clone, Repository, CloneOptions } from "nodegit";
-
-import { spawn, spawnSync, execFile }  from "child_process";
-import WebTorrent = require("webtorrent");
-import fs = require("fs");
-import path = require('path');
-import pty = require('node-pty');
+import commander = require('commander');
 import storage = require('node-persist');
+import inquirer = require('inquirer');
+import * as git from 'isomorphic-git';
+import fs = require('fs');
+import WebTorrent = require("webtorrent");
+import * as cliProgress from 'cli-progress';
+import { spawn, execFile, exec } from 'child_process';
+import { Spinner } from 'cli-spinner';
+
+git.plugins.set('fs', fs);
+
+commander
+    .command('run [game]')
+    .description('Play a game.')
+    .action(function (game) {
+        console.log("This command doesn't work yet!");
+    });
+
+commander
+    .command('install [game]')
+    .description('Install a game.')
+    .action(function (game) {
+        if (!game) {
+            console.log("No game detected.");
+        }
+        console.log("This command doesn't work yet!");
+    });
+
+const WelcomeText = 'Welcome to coldcut, the installer for AI text adventure games.';
+
+const InstalledGamesKey = 'installedGames';
+
+const SupportedGames = [
+    'AIDungeon'
+];
+
+const AIDungeonRepoPath = './repos/AIDungeon';
 
 async function main() {
+
     await storage.init({
         dir: 'system'
     });
 
-    console.log("initialized system storage");
+    const storageKeys = await storage.keys();
 
-    const keys = await storage.keys();
-
-    let needsInstallation = true;
-    if (keys.includes('hasCompleteInstallation')) {
-        let hasCompleteInstallation = await storage.getItem('hasCompleteInstallation');
-        needsInstallation = !hasCompleteInstallation;
-
-        spawnGame();
+    let installedGames: string[] = [];
+    if (storageKeys.includes(InstalledGamesKey)) {
+        installedGames = await storage.getItem(InstalledGamesKey);
     }
 
-    if (needsInstallation) {
-        // TODO - may be a bit faster if depth=1
-        // TODO - always checkout specific commit to guarantee it's functional
-        let firstRepo: Promise<void> = Clone.clone("https://github.com/AIDungeon/AIDungeon", "./repos/AIDungeon")
-        .then(function (repo: Repository) {
-            console.log("done downloading AI Dungeon");
-        })
-        .catch(function (reason) {
-            console.log("error cloning AIDungeon", JSON.stringify(reason))
-        });
+    commander.parse(process.argv);
 
-        let secondRepo: Promise<void> = Clone.clone("https://github.com/thadunge2/AIDungeon.git", "./repos/thadunge2").then(function (repo: Repository) {
-            console.log("done downloading ThaDunge2");
-        })
-        .catch(function (reason) {
-            console.log("error cloning ThaDunge2", JSON.stringify(reason))
-        });
+    if (process.argv.length === 2) {
+        console.log(WelcomeText);
 
-        Promise.all([firstRepo, secondRepo]).then(function () {
-            console.log("both repos finished clone operation");
-        
-            // python -m venv tutorial_env
-            const ls = spawn('python', ['-m', 'venv', 'venv'], {
-                cwd: "./repos/AIDungeon"
-            });
+        if (installedGames.length === 0) {
+            console.log('No installations detected.');
 
-            ls.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-            });
-            
-            ls.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-            });
-            
-            ls.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
+            let installGameChoices = SupportedGames.concat('(Cancel)');
+            let questions = [
+                {
+                    type: 'list',
+                    name: 'game',
+                    message: 'Which game to install?',
+                    choices: installGameChoices
+                }
+            ];
 
-            // install --upgrade pip setuptools
-            const pip1 = execFile("./venv/bin/pip", ["install", "--upgrade", "pip", "setuptools"], {
-                cwd: "./repos/AIDungeon"
-            });
+            let answers = await inquirer.prompt(questions);
+            if (answers['game'] == "(Cancel)") {
+                process.exit(0);
+            }
 
-            pip1.on('close', (code) => {
-                console.log(`child process exited with code ${code}`);
-
-                // install -r "${BASE_DIR}/requirements.txt"
-                const pip2 = execFile("./venv/bin/pip", ["install", "-r", "requirements.txt"], {
-                    cwd: "./repos/AIDungeon"
+            if (answers['game'] == 'AIDungeon') {
+                answers = await inquirer.prompt({
+                    type: 'confirm',
+                    name: 'installConfirmation',
+                    message: 'This game will take up 6.25 GB of disk space. Continue?',
+                    default: false
                 });
+            }
 
-                pip2.on('close', (code) => {
-                    console.log(`child process exited with code ${code}`);
+            if (answers['installConfirmation'] === false) {
+                process.exit(0);
+            }
 
-                    // start torrent
-                    const client = new WebTorrent();
+            let spinner = new Spinner('%s Installing AIDungeon game files');
+            spinner.start();
 
-                    const torrentId = "magnet:?xt=urn:btih:b343b83b35bff774dab13e0281ce13b3daf37d3e&dn=model_v5";
+            const AIDungeonModelPath = './repos/AIDungeon/generator/gpt2/models';
 
-                    let torrent = client.add(torrentId, {
-                        path: "./models/default"
-                    });
+            await git.clone({
+                dir: AIDungeonRepoPath,
+                url: "https://github.com/AIDungeon/AIDungeon",
+                ref: 'master',
+                singleBranch: true,
+                depth: 1,
+                noSubmodules: true
+            });
 
-                    torrent.on('ready', function () {
-                        console.log("torrent: ready");
-                    });
+            const makeVenv = spawn('../../python/python.exe', ['-m', 'venv', 'venv'], {
+                cwd: AIDungeonRepoPath
+            });
 
-                    torrent.on('warning', function (err) {
-                        console.log("torrent: warning", JSON.stringify(err));
-                    });
+            // TODO - add debug flag and emit stderr
 
-                    torrent.on('error', function (err) {
-                        console.log("torrent: error", JSON.stringify(err));
-                    });
+            // makeVenv.stderr.on('data', (data) => {
+            //     console.error(`stderr: ${data}`);
+            // });
 
-                    torrent.on('done', function () {
-                        console.log("torrent: done");
+            const makeVenvPromise = new Promise((resolve, reject) => {
+                makeVenv.on('close', resolve);
+            });
 
-                        client.destroy(function () {
-                            console.log("closing client");
+            await makeVenvPromise;
 
-                            fs.mkdir('./repos/AIDungeon/generator/gpt2/models', { recursive: true }, (err) => {
-                                if (err) throw err;
+            const upgradePip = execFile("./venv/Scripts/pip", ["install", "--upgrade", "pip", "setuptools", "--user"], {
+                cwd: AIDungeonRepoPath
+            });
 
-                                console.log("created models dir");
+            // upgradePip.stderr.on('data', (data) => {
+            //     console.error(`stderr: ${data}`);
+            // });
 
-                                let destinationLink = path.resolve(process.cwd(), 'models/default/model_v5');
-                                fs.symlink(destinationLink, "./repos/AIDungeon/generator/gpt2/models/model_v5", (err) => {
-                                    if (err) {
-                                        if (err.code === 'EEXIST') {
-                                            console.log("symlink already present");
-                                        } else {
-                                            throw err;
-                                        }
-                                    } 
+            const upgradePipPromise = new Promise((resolve, reject) => {
+                upgradePip.on('close', resolve);
+            });
 
-                                    console.log("created symlink for model_v5");
+            await upgradePipPromise;
 
-                                    storage.setItem('hasCompleteInstallation', true).then(function (result) {
-                                        console.log("updated system storage to mark complete installation");
+            const installRequirements = execFile("./venv/Scripts/pip", ["install", "-r", "requirements.txt"], {
+                cwd: AIDungeonRepoPath
+            });
 
-                                        spawnGame();
-                                    });
-                                });
-                            });
-                        });
-                    });
+            // installRequirements.stderr.on('data', (data) => {
+            //     console.error(`stderr: ${data}`);
+            // });
 
-                    let downloadStatsRateLimiter = 0;
-                    const interval = 100;
-                    torrent.on('download', function (bytes) {
-                        if (downloadStatsRateLimiter > interval) {
-                            console.log('just downloaded:', bytes, '| total downloaded:', torrent.downloaded, '| speed:', torrent.downloadSpeed, '| progress:', torrent.progress * 100 + "%");
+            const installRequirementsPromise = new Promise((resolve, reject) => {
+                installRequirements.on('close', resolve);
+            });
 
-                            downloadStatsRateLimiter = 0;
-                        }
-                        
-                        downloadStatsRateLimiter++;
-                    });
+            await installRequirementsPromise;
 
-                    torrent.on('upload', function (bytes) {
-                        console.log("torrent: upload", JSON.stringify(bytes));
-                    });
+            await fs.promises.mkdir(AIDungeonModelPath, { recursive: true });
+
+            spinner.stop(true);
+            console.log('Completed installing game files.');
+            console.log('Downloading AI model...');
+
+            const torrentClient = new WebTorrent();
+            const torrentId = "magnet:?xt=urn:btih:b343b83b35bff774dab13e0281ce13b3daf37d3e&dn=model_v5";
+
+            const progressBar = new cliProgress.SingleBar({
+                hideCursor: true,
+                format: '{bar} {percentage}% | {value}/{total}'
+            }, cliProgress.Presets.shades_classic);
+
+            let torrent = torrentClient.add(torrentId, {
+                path: AIDungeonModelPath
+            });
+
+            // TODO -- need to get rid of EventEmitter issues.
+            torrent.setMaxListeners(Infinity);
+
+            torrent.on('metadata', () => {
+                progressBar.start(torrent.length, torrent.downloaded);
+
+            });
+
+            torrent.on('download', (bytes) => {
+                progressBar.update(torrent.downloaded);
+
+            });
+
+            let torrentDonePromise = new Promise((resolve, reject) => {
+                torrent.on('done', () => {
+                    progressBar.stop();
+
+                    torrentClient.destroy();
+
+                    resolve();
                 });
             });
-            });
-        });
+
+            await torrentDonePromise;
+
+            installedGames.push('AIDungeon');
+            await storage.setItem(InstalledGamesKey, installedGames);
+
+            console.log("\nDone installing! Run coldcut again to play.");
+        } else {
+            console.log('\nSelect a game to run.\n');
+
+            let runGameChoices = installedGames.concat(['(Cancel)']);
+            let questions = [
+                {
+                    type: 'list',
+                    name: 'game',
+                    message: 'Select a game to run.',
+                    choices: runGameChoices
+                }
+            ];
+
+            let answers = await inquirer.prompt(questions);
+            if (answers['game'] == "(Cancel)") {
+                process.exit(0);
+            }
+
+            if (answers['game'] == 'AIDungeon') {
+                exec("start cmd.exe /K \".\\venv\\Scripts\\python play.py\"", {
+                    cwd: AIDungeonRepoPath
+                });
+            }
+        }
     }
 }
 
-function spawnGame() {
-    var ptyProcess = pty.spawn("./venv/bin/python", ['play.py'], {
-        name: 'xterm-color',
-        cols: 80,
-        rows: 30,
-        cwd: "./repos/AIDungeon",
-        env: process.env
-    });
-
-    process.stdin.on('data', function (data: any) {
-        ptyProcess.write(data);
-    });
-    
-    ptyProcess.on('data', function(data: any) {
-        process.stdout.write(data);
-    });
-
-    ptyProcess.on('exit', function(exitCode: number, signal?: number) {
-        console.log("exiting", exitCode, signal);
-        process.exit();
-    });
+function caseInsensitiveEquals(a: string, b: string) {
+    return (a.localeCompare(b, undefined, { sensitivity: 'base'}) === 0);
 }
 
 main();
